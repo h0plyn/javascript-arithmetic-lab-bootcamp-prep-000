caf1931cc5cc65814f57db86313a7c0f3df46bb4
Ricky Rhodes <ricky@rickyrhodes.com>
2020-08-03 00:42:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,4 +22,24 @@ function subtract(a, b) {
   return b - a;
 };
 
-function multi
+function multiply(a, b) {
+  return a * b;
+};
+
+function divide (a, b) {
+  return a / b;
+};
+
+function increment(n) {
+  return n++;  
+}
+
+function decrement(n) {
+  return n--;
+};
+
+
+
+
+
+
