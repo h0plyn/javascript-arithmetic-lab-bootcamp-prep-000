dccb1213fc97708a5fdf80c33a0d3f23e989086c
Ricky Rhodes <ricky@rickyrhodes.com>
2020-08-03 00:50:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ function decrement(n) {
   return n -= 1;
 };
 
+function makeInt(string) {
+  return parseInt(string);
+};
+
+function preserveDecimal(string) {
+  return parseFloat(string);
+};
 
 
 
